199090d1a6256d9e5fbdecef91463e0c18e9ae6e
qberg <qbergnoe@gmail.com>
2025-04-05 04:46:12 +0530

    fixed styles of contact form

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,9 +8,10 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
       <input
         type={type}
         className={cn(
-          'flex h-9 w-full rounded-md border border-input-border bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+          'flex w-full rounded-md border border-input-border bg-transparent px-3 py-1 shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 text-s',
           className,
         )}
+        style={{ height: 'clamp(2rem, 1.6139rem + 1.5717vw, 3.5rem)' }}
         ref={ref}
         {...props}
       />
--- src/components/ui/text-area.tsx
+++ src/components/ui/text-area.tsx
@@ -7,11 +7,11 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'tex
     return (
       <textarea
         className={cn(
-          'flex w-full rounded-md border border-input-border bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+          'flex w-full rounded-md border border-input-border bg-transparent px-3 py-2 text-s shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
           className,
         )}
         style={{
-          minHeight: 'clamp(1rem, -1.5737rem + 10.4781vw, 11rem)',
+          minHeight: 'clamp(1.5rem, -0.1085rem + 6.5488vw, 7.75rem)',
         }}
         ref={ref}
         {...props}
