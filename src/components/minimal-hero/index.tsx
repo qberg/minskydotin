8c8e9b28c1bac9f1fce35f11e81dc182c071d228
qberg <qbergnoe@gmail.com>
2025-04-09 08:41:09 +0530

    fixed minor padding issues of portfolio and blog page

--- src/components/minimal-hero/index.tsx
+++ src/components/minimal-hero/index.tsx
@@ -7,11 +7,13 @@ const MinimalHero = ({ title, count }: MinimalHeroProps) => {
   const safeCount = typeof count === 'number' ? count : 0
   const formattedCount = safeCount < 10 ? `0${safeCount}` : `${safeCount}`
   return (
-    <section className="relative">
-      <div
-        className="w-full max-w-[1760px] mx-auto flex flex-col"
-        style={{ padding: 'clamp(1.5rem, 1.5vw, 2.5rem)', gap: 'clamp(1rem, 4vw, 3.5rem)' }}
-      >
+    <section
+      className="relative"
+      style={{
+        padding: 'clamp(1.5rem, 0.2775rem + 4.9771vw, 6.25rem)',
+      }}
+    >
+      <div className="w-full max-w-[1720px] mx-auto flex flex-col gap-14">
         <h2>
           {title}
           <sup className="text-secondary">[{formattedCount}]</sup>
--- src/components/sections/portfolio/projects-grid.tsx
+++ src/components/sections/portfolio/projects-grid.tsx
@@ -3,25 +3,30 @@ import allProjects from '@/data/projects/index'
 
 const ProjectsGrid = () => {
   return (
-    <section
-      className="flex flex-col items-center justify-center relative w-full max-w-[1760px] mx-auto"
+    <div
+      className="w-full mx-auto relative"
       style={{
-        padding: 'clamp(1.5rem, 1.5vw, 2.5rem)',
-        gap: 'clamp(1rem, 0.072rem + 3.778vw, 3rem)',
+        padding: 'clamp(1.5rem, 0.2775rem + 4.9771vw, 6.25rem)',
       }}
     >
-      <div className="w-full flex items-center justify-end">Filter</div>
-      <div
-        className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3"
+      <section
+        className="flex flex-col items-center justify-center relative w-full max-w-[1720px] mx-auto"
         style={{
-          gap: 'clamp(4rem, -16rem + 25vw, 14rem)',
+          gap: 'clamp(1rem, 0.072rem + 3.778vw, 3rem)',
         }}
       >
-        {allProjects.map((project, index) => (
-          <ProjectCard key={index} {...project.card} slug={project.slug} />
-        ))}
-      </div>
-    </section>
+        <div
+          className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3"
+          style={{
+            gap: 'clamp(4rem, -16rem + 25vw, 14rem)',
+          }}
+        >
+          {allProjects.map((project, index) => (
+            <ProjectCard key={index} {...project.card} slug={project.slug} />
+          ))}
+        </div>
+      </section>
+    </div>
   )
 }
 
