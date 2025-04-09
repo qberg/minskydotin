3f7b36ed6edbfc6b0cd82d541617a0127a7a4342
qberg <qbergnoe@gmail.com>
2025-04-09 11:59:54 +0530

    fixed scale

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -7,9 +7,20 @@ import Footer from '@/components/footer'
 import { Toaster } from '@/components/ui/sonner'
 import Navbar from '@/components/navbar'
 
+import type { Viewport } from 'next'
+
 export const metadata = {
   description: 'Digital presence of Minsky labs',
   title: 'Minsky Labs',
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 0.75,
+  maximumScale: 0.75,
+  userScalable: false,
+  // Also supported but less commonly used
+  // interactiveWidget: 'resizes-visual',
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
